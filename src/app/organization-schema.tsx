d3c2967f9be6fc919c2b6bdb14618d4801493e23
Marta Muurniece <marta.muurniece@gmail.com>
2026-08-05 12:53:38 +0000

    Add sameAs social profiles to the Organization JSON-LD

--- src/app/organization-schema.tsx
+++ src/app/organization-schema.tsx
@@ -7,6 +7,13 @@ export default function OrganizationSchema() {
     logo: 'https://coride.org/assets/coride-logo.png',
     description:
       'Verified workplace carpooling that gets co-workers to work together and rewards drivers and passengers with perks from local businesses.',
+    sameAs: [
+      'https://www.linkedin.com/company/corideapp',
+      'https://www.instagram.com/corideapp',
+      'https://x.com/corideapp',
+      'https://www.facebook.com/corideapp',
+      'https://www.tiktok.com/@corideapp',
+    ],
   }
   return (
     <script
